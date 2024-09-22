13f92711ad969e476489b7358b670587769801cd
Jacek Skarupa <jacek.skarupa@mindsailors.com>
2024-09-22 13:23:54 +0200

    mismatched library in platformio, switching to arduino ide

--- src/main.cpp
+++ src/main.cpp
@@ -99,49 +99,66 @@ void drive_actuator(Actuators motor, uint8_t direction, uint16_t speed) {
   }
 }
 
-void onConnectedController(ControllerPtr controller) {
-  bool foundEmptySlot = false;
-  for (int i = 0; i < BP32_MAX_GAMEPADS; i++)
-  {
-    if (myControllers[1] == nullptr)
-    {
-      Serial.print("CALLBACK: Controller is connected, index=");
-      Serial.println(i);
-      myControllers[i] = controller;
-      foundEmptySlot = true;
-      
-      ControllerProperties properties = controller->getProperties();
-      char buf[80];
-      sprintf(buf, 
-                "BTAddr: %02x:%02x:%02x:%02x:%02x:%02x, VID/PID: %04x/%04x, ",
-                "flags: 0x%02x",
-                properties.btaddr[0], properties.btaddr[1], properties.btaddr[2],
-                properties.btaddr[3], properties.btaddr[4], properties.btaddr[5],
-                properties.vendor_id, properties.product_id, properties.flags);
-      Serial.println(buf);
-      break;
+// This callback gets called any time a new gamepad is connected.
+// Up to 4 gamepads can be connected at the same time.
+void onConnectedController(ControllerPtr ctl) {
+    bool foundEmptySlot = false;
+    for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
+        if (myControllers[i] == nullptr) {
+            Serial.printf("CALLBACK: Controller is connected, index=%d\n", i);
+            // Additionally, you can get certain gamepad properties like:
+            // Model, VID, PID, BTAddr, flags, etc.
+            ControllerProperties properties = ctl->getProperties();
+            Serial.printf("Controller model: %s, VID=0x%04x, PID=0x%04x\n", ctl->getModelName().c_str(), properties.vendor_id,
+                           properties.product_id);
+            myControllers[i] = ctl;
+            foundEmptySlot = true;
+            break;
+        }
     }
-  }
-  if (!foundEmptySlot)
-  {
-    Serial.println("CALLBACK: Controller connected but could not found empty slot");
-  }
+    if (!foundEmptySlot) {
+        Serial.println("CALLBACK: Controller connected, but could not found empty slot");
+    }
 }
 
-void onDisconnectedController(ControllerPtr controller) {
-  bool foundGamepad = false;
+void onDisconnectedController(ControllerPtr ctl) {
+    bool foundController = false;
 
-  for (int i = 0; i < BP32_MAX_GAMEPADS; i++)
-  {
-    if (myControllers[i] == controller)
-    {
-      Serial.print("CALLBACK: Controller is disconnected from index=");
-      Serial.println(i);
-      myControllers[i] = nullptr;
-      foundGamepad = true;
-      break;
+    for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
+        if (myControllers[i] == ctl) {
+            Serial.printf("CALLBACK: Controller disconnected from index=%d\n", i);
+            myControllers[i] = nullptr;
+            foundController = true;
+            break;
+        }
     }
-  }
+
+    if (!foundController) {
+        Serial.println("CALLBACK: Controller disconnected, but not found in myControllers");
+    }
+}
+
+void dumpGamepad(ControllerPtr ctl) {
+    Serial.printf(
+        "idx=%d, dpad: 0x%02x, buttons: 0x%04x, axis L: %4d, %4d, axis R: %4d, %4d, brake: %4d, throttle: %4d, "
+        "misc: 0x%02x, l1:%1x, r1:%1x, b:%1x\n",
+        ctl->index(),        // Controller Index
+        ctl->dpad(),         // D-pad
+        ctl->buttons(),      // bitmask of pressed buttons
+        ctl->axisX(),        // (-511 - 512) left X Axis
+        ctl->axisY(),        // (-511 - 512) left Y axis
+        ctl->axisRX(),       // (-511 - 512) right X axis
+        ctl->axisRY(),       // (-511 - 512) right Y axis
+        ctl->brake(),        // (0 - 1023): brake button
+        ctl->throttle(),     // (0 - 1023): throttle (AKA gas) button
+        ctl->miscButtons(),  // bitmask of pressed "misc" buttons
+        //
+        ctl->l1(),
+        // ctl->l2(),
+        ctl->r1(),
+        // ctl->r2(),
+        ctl->b()
+    );
 }
 
 void processGamepad(ControllerPtr gamepad) {
@@ -227,21 +244,32 @@ void setup() {
 void loop() {
   Serial1.println("Hello, World!");
 
-  BP32.update();
+  // This call fetches all the controllers' data.
+  // Call this function in your main loop.
 
-  for (int i = 0; i < BP32_MAX_CONTROLLERS; i++)
-  {
-    ControllerPtr myController = myControllers[i];
-    Serial.println("Looping...");
+  /*
+  BUG in Arduino IDE bluepad32 is a different library and thus this code doesnt compile and run from platformio
+  BUG Because of that, the code development is continued as a separate project in Arduino IDE
+  */
+  bool dataUpdated = BP32.update();
+  if (dataUpdated)
+      processControllers();
 
-    if (myController && myController->isConnected())
-    {
-      if (myController->isGamepad())
-      {
-        processGamepad(myController);
-        Serial.println("Gamepad");
-    }
-  }
+  // BP32.update();
+
+  // for (int i = 0; i < BP32_MAX_CONTROLLERS; i++)
+  // {
+  //   ControllerPtr myController = myControllers[i];
+  //   Serial.println("Looping...");
+
+  //   if (myController && myController->isConnected())
+  //   {
+  //     if (myController->isGamepad())
+  //     {
+  //       processGamepad(myController);
+  //       Serial.println("Gamepad");
+  //   }
+  // }
   
   delay(150);
 
@@ -263,5 +291,5 @@ void loop() {
   // drive_actuator(THUMB, 1, 2048);
   // drive_actuator(AUX, 1, 2048);
   // delay(1000);
-// }
+}
 }
